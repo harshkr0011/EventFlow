24e3a1817e2d77afbb6f08a3741e76454edaa0de
Harsh Kumar <harshkr0011@gmail.com>
2025-09-26 12:10:19 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/client-firebase.ts
+++ src/lib/client-firebase.ts
@@ -5,12 +5,13 @@ import { getAuth } from 'firebase/auth';
 // This is a public configuration and is safe to expose.
 // Security is handled by Firebase Security Rules and App Check.
 const firebaseConfig = {
-  projectId: 'studio-4758176413-e97c3',
-  appId: '1:125591351417:web:6d59249e06dddd45d8a4a8',
-  apiKey: 'AIzaSyAWZOQcphfG9k1Rg5-aBK_e9n3sN6e3_8k',
-  authDomain: 'studio-4758176413-e97c3.firebaseapp.com',
-  measurementId: '',
-  messagingSenderId: '125591351417',
+  apiKey: "AIzaSyAWZOQcphfG9k1Rg5-aBK_e9n3sN6e3_8k",
+  authDomain: "studio-4758176413-e97c3.firebaseapp.com",
+  projectId: "studio-4758176413-e97c3",
+  storageBucket: "studio-4758176413-e97c3.appspot.com",
+  messagingSenderId: "125591351417",
+  appId: "1:125591351417:web:6d59249e06dddd45d8a4a8",
+  measurementId: "G-1W5T5W4Q6P"
 };
 
 let app: FirebaseApp;
