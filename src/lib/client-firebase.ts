58afef3a791b21c70f20c810f30e16a9db02e1b7
Harsh Kumar <harshkr0011@gmail.com>
2025-09-26 12:18:11 +0000

    than why the login and sign up function is not working

--- src/lib/client-firebase.ts
+++ src/lib/client-firebase.ts
@@ -6,9 +6,9 @@ import { getAuth } from 'firebase/auth';
 // Security is handled by Firebase Security Rules and App Check.
 const firebaseConfig = {
   apiKey: "AIzaSyAWZOQcphfG9k1Rg5-aBK_e9n3sN6e3_8k",
-  authDomain: "studio-4758176413-e97c3.firebaseapp.com",
-  projectId: "studio-4758176413-e97c3",
-  storageBucket: "studio-4758176413-e97c3.appspot.com",
+  authDomain: "studio-4758176413-e9c3.firebaseapp.com",
+  projectId: "studio-4758176413-e9c3",
+  storageBucket: "studio-4758176413-e9c3.appspot.com",
   messagingSenderId: "125591351417",
   appId: "1:125591351417:web:6d59249e06dddd45d8a4a8",
   measurementId: "G-1W5T5W4Q6P"
