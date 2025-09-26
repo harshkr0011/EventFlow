66b16eec4828099faa1d222711eed5e85ac357e3
Harsh Kumar <harshkr0011@gmail.com>
2025-09-26 11:31:15 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@
 import { initializeApp, getApps, getApp, type FirebaseApp } from 'firebase/app';
 
 const firebaseConfig = {
-  projectId: 'studio-4758176413-e97c3',
-  appId: '1:125591351417:web:6d59249e06dddd45d8a4a8',
-  apiKey: 'AIzaSyAWZOQcphfG9k1Rg5-aBK_e9n3sN6e3_8k',
-  authDomain: 'studio-4758176413-e97c3.firebaseapp.com',
-  measurementId: '',
-  messagingSenderId: '125591351417'
+  "projectId": "studio-4758176413-e97c3",
+  "appId": "1:125591351417:web:6d59249e06dddd45d8a4a8",
+  "apiKey": "AIzaSyAWZOQcphfG9k1Rg5-aBK_e9n3sN6e3_8k",
+  "authDomain": "studio-4758176413-e97c3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "125591351417"
 };
 
 // Initialize Firebase
