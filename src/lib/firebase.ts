f0151884fafcf88acc0e79cbc7bf88cdd087fa4d
Harsh Kumar <harshkr0011@gmail.com>
2025-09-26 10:18:33 +0000

    Running in emulator mode. Do not use with production credentials

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,23 +2,25 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth, connectAuthEmulator } from 'firebase/auth';
 
+const runningInEmulator = process.env.NODE_ENV === 'development';
+
 const firebaseConfig = {
-  projectId: 'studio-4758176413-e97c3',
-  appId: '1:125591351417:web:6d59249e06dddd45d8a4a8',
-  apiKey: 'AIzaSyAWZOQcphfG9k1Rg5-aBK_e9n3sN6e3_8k',
-  authDomain: 'studio-4758176413-e97c3.firebaseapp.com',
+  projectId: runningInEmulator ? 'demo-project' : 'studio-4758176413-e97c3',
+  appId: runningInEmulator ? 'demo-app-id' : '1:125591351417:web:6d59249e06dddd45d8a4a8',
+  apiKey: runningInEmulator ? 'demo-api-key' : 'AIzaSyAWZOQcphfG9k1Rg5-aBK_e9n3sN6e3_8k',
+  authDomain: runningInEmulator ? 'localhost' : 'studio-4758176413-e97c3.firebaseapp.com',
   measurementId: '',
-  messagingSenderId: '125591351417'
+  messagingSenderId: runningInEmulator ? 'demo-messaging-sender-id' : '125591351417'
 };
+
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 
-if (process.env.NODE_ENV !== 'production' && typeof window !== 'undefined') {
+if (runningInEmulator && typeof window !== 'undefined') {
     // In a development environment, connect to the local Firebase Auth emulator
-    // Note: The host for the emulator is automatically determined by the Firebase SDK
     try {
-        connectAuthEmulator(auth, "http://localhost:9099");
+        connectAuthEmulator(auth, "http://127.0.0.1:9099", { disableWarnings: true });
         console.log("Connected to Firebase Auth Emulator");
     } catch (e) {
         console.error("Failed to connect to Firebase Auth Emulator", e);
