e7fa4a8074b8102673598423ab35bd12280b84cc
Harsh Kumar <harshkr0011@gmail.com>
2025-09-26 11:42:17 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/lib/client-firebase.ts
+++ src/lib/client-firebase.ts
@@ -1,8 +1,17 @@
-
 'use client';
 import { initializeApp, getApp, getApps, type FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { firebaseConfig } from './firebase';
+
+// This is a public configuration and is safe to expose.
+// Security is handled by Firebase Security Rules and App Check.
+const firebaseConfig = {
+  projectId: 'studio-4758176413-e97c3',
+  appId: '1:125591351417:web:6d59249e06dddd45d8a4a8',
+  apiKey: 'AIzaSyAWZOQcphfG9k1Rg5-aBK_e9n3sN6e3_8k',
+  authDomain: 'studio-4758176413-e97c3.firebaseapp.com',
+  measurementId: '',
+  messagingSenderId: '125591351417',
+};
 
 let app: FirebaseApp;
 if (!getApps().length) {
