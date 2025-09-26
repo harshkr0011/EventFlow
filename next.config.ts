d35f6f4675b2652dde2a6a0f61416244d0ad8990
Harsh Kumar <harshkr0011@gmail.com>
2025-09-26 08:34:10 +0000

    this will be the url for holi event image = https://blogs.tripzygo.in/wp

--- next.config.ts
+++ next.config.ts
@@ -35,6 +35,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'blogs.tripzygo.in',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
