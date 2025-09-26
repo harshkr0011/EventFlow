90b4d45eff1edec562a87140552df108c652ee87
Harsh Kumar <harshkr0011@gmail.com>
2025-09-26 08:54:04 +0000

    Indian Premier League (IPL) Final image url =https://newsarenaindia.com/

--- next.config.ts
+++ next.config.ts
@@ -53,6 +53,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.newsarenaindia.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
