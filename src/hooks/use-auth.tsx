837bdd73ddc3301bc2e599afe84fa1f260aaa493
Harsh Kumar <harshkr0011@gmail.com>
2025-09-26 10:33:35 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/hooks/use-auth.tsx
+++ src/hooks/use-auth.tsx
@@ -25,7 +25,6 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
   const [user, setUser] = useState<User | null>(null);
   const [loading, setLoading] = useState(true);
   const router = useRouter();
-  const pathname = usePathname();
   const auth = getAuth(app);
 
   useEffect(() => {
